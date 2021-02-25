54ab86cae54b131f0d0d6611eff92ff3ceaa6a28
Arthur Parsons <artbdev@Arthurs-MacBook-Pro-2.local>
2021-02-24 21:05:38 -0500

    basic setup, stack repl, build, holes, ghcid, commments

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,19 @@
+-- single line comment
+{-
+    multi-line coment 
+-}
+{-
+    holes:
+        funcName _ then save
+        compiler will give you hint
+        on what type the function takes
+
+    stack repl : to get interactive haskell console
+        :q quit the console
+        :h help
+
+    ghcid : command to get compiler to wacth file for errors
+-}
 module Lib
     ( someFunc
     ) where
